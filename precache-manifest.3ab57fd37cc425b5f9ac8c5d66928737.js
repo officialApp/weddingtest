ab880b16fe8d0b7490c27e227422eda0bcb917b8
AkbarMaliki <taufikakbarmalikitkj@gmail.com>
2021-09-09 23:53:47 +0800

    peta

---
+++ precache-manifest.3ab57fd37cc425b5f9ac8c5d66928737.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bf35af64ca7369a6fa6b0849643bf5",
+    "url": "404.html"
+  },
+  {
+    "revision": "d6919814097c41f103aebe7b5f196d6b",
+    "url": "android-launchericon-192-192.png"
+  },
+  {
+    "revision": "c80f68ccaf71bb2f96115e536335c439",
+    "url": "android-launchericon-512-512.png"
+  },
+  {
+    "revision": "839ea1c1e0439ecadcb5",
+    "url": "css/app~bc5f445c.e5091971.css"
+  },
+  {
+    "revision": "ee631fff5d336573a961",
+    "url": "css/app~d0ae3f07.429e50b6.css"
+  },
+  {
+    "revision": "dd864f18539226808639",
+    "url": "css/chunk-62141781.11452e69.css"
+  },
+  {
+    "revision": "a4ac951e6eb374e35b68",
+    "url": "css/chunk-97903dc2.df02bd30.css"
+  },
+  {
+    "revision": "fac4bb5120ec69bdadd2",
+    "url": "css/chunk-af48f40c.30a57960.css"
+  },
+  {
+    "revision": "4dd6910b617250315cc5",
+    "url": "css/chunk-vendors~4a8239e8.e61d5c10.css"
+  },
+  {
+    "revision": "b03624f8304b62b65dd5",
+    "url": "css/chunk-vendors~5068d5f8.582dcb44.css"
+  },
+  {
+    "revision": "8fc39ac25752e9ea3042",
+    "url": "css/chunk-vendors~e047c002.bd659205.css"
+  },
+  {
+    "revision": "9613377fcdbec94dccac",
+    "url": "css/page-admin-nikah-acara~page-admin-nikah-comment~page-admin-nikah-galery~page-admin-nikah-index~page-~d6cec0eb.404255ab.css"
+  },
+  {
+    "revision": "0532b9631b0f82dd5856",
+    "url": "css/page-test~fcee1841.8ffa37d0.css"
+  },
+  {
+    "revision": "8de06593dc73935da8be",
+    "url": "css/page-undangan-index~31ecd969.5da6ce16.css"
+  },
+  {
+    "revision": "4fdd273d1751b4ef15868099e07f59f9",
+    "url": "draw1.jpeg"
+  },
+  {
+    "revision": "a0fc6c24d1f3ff9ac281887c92b24acd",
+    "url": "fastclick.min.js"
+  },
+  {
+    "revision": "7e1c5e669eb776c4bdc7e949f3e78471",
+    "url": "firebase-messaging-sw.js"
+  },
+  {
+    "revision": "29f9630f7d87a79830d1c321e1600f2e",
+    "url": "fonts/typicons.29f9630f.ttf"
+  },
+  {
+    "revision": "95aa28e29618c068e8a53f64c87cb6a9",
+    "url": "fonts/typicons.95aa28e2.woff"
+  },
+  {
+    "revision": "a509074c88f07e40a8d2f158c4fcdda8",
+    "url": "fonts/typicons.a509074c.eot"
+  },
+  {
+    "revision": "45c525d2e0832d3f6ee8bc3dcdb103ee",
+    "url": "img/couple2.45c525d2.png"
+  },
+  {
+    "revision": "cae8fddf5efaa40d4b30d3ec57f7d979",
+    "url": "img/typicons.cae8fddf.svg"
+  },
+  {
+    "revision": "77dc850aba2946df15f2065430746f7c",
+    "url": "index.html"
+  },
+  {
+    "revision": "839ea1c1e0439ecadcb5",
+    "url": "js/app~bc5f445c.ef50d590.js"
+  },
+  {
+    "revision": "ee631fff5d336573a961",
+    "url": "js/app~d0ae3f07.c04fcc25.js"
+  },
+  {
+    "revision": "dd864f18539226808639",
+    "url": "js/chunk-62141781.69bdd468.js"
+  },
+  {
+    "revision": "a4ac951e6eb374e35b68",
+    "url": "js/chunk-97903dc2.d841e741.js"
+  },
+  {
+    "revision": "fac4bb5120ec69bdadd2",
+    "url": "js/chunk-af48f40c.38a4c7a6.js"
+  },
+  {
+    "revision": "87c54cb38a6efa8ef79e",
+    "url": "js/chunk-cb8bd7c4.9a5877fd.js"
+  },
+  {
+    "revision": "24f68ef157e88e34ac92",
+    "url": "js/chunk-vendors~074dc65d.e16b67ac.js"
+  },
+  {
+    "revision": "6337f1c0d7cfb61bfe9d",
+    "url": "js/chunk-vendors~0f08bdbc.80dbe8fc.js"
+  },
+  {
+    "revision": "d34f0d5c49881a768ac3",
+    "url": "js/chunk-vendors~0f485567.ec7185d3.js"
+  },
+  {
+    "revision": "548aef562c35dae7fb72",
+    "url": "js/chunk-vendors~18eda011.35c9fe77.js"
+  },
+  {
+    "revision": "257f2c924043765d8205",
+    "url": "js/chunk-vendors~1c3a2c3f.17fb4142.js"
+  },
+  {
+    "revision": "e962ffec30f6335f7cc2",
+    "url": "js/chunk-vendors~1f20a385.32f04333.js"
+  },
+  {
+    "revision": "f6d424fecccb1e0943a7",
+    "url": "js/chunk-vendors~1fb195ae.4c80839f.js"
+  },
+  {
+    "revision": "4dd6910b617250315cc5",
+    "url": "js/chunk-vendors~4a8239e8.8c9338d5.js"
+  },
+  {
+    "revision": "b03624f8304b62b65dd5",
+    "url": "js/chunk-vendors~5068d5f8.793096b9.js"
+  },
+  {
+    "revision": "9590a9a6785b84eae9ed",
+    "url": "js/chunk-vendors~670e76f6.050ee35d.js"
+  },
+  {
+    "revision": "992f8aeb03a05e1e5a0f",
+    "url": "js/chunk-vendors~678675a8.94479e33.js"
+  },
+  {
+    "revision": "dc93b4669ad3583552d2",
+    "url": "js/chunk-vendors~6ff199a4.e10495b2.js"
+  },
+  {
+    "revision": "65d151812b2e6c6ae1ec",
+    "url": "js/chunk-vendors~73081ec5.39d1d0d4.js"
+  },
+  {
+    "revision": "98b5be89f9dd619fa6a6",
+    "url": "js/chunk-vendors~77bf5e45.b1c7ff19.js"
+  },
+  {
+    "revision": "641533e8f38d13e74125",
+    "url": "js/chunk-vendors~7ffa8a2f.95811097.js"
+  },
+  {
+    "revision": "f34492276cd2cee05e52",
+    "url": "js/chunk-vendors~8aaa0a96.842e3cf8.js"
+  },
+  {
+    "revision": "69539ee2ac10fbde697f",
+    "url": "js/chunk-vendors~987e6011.70e60ade.js"
+  },
+  {
+    "revision": "a084789c3a4641021b90",
+    "url": "js/chunk-vendors~9c4131f8.3026e3d0.js"
+  },
+  {
+    "revision": "c75cc4be4237171aeff1",
+    "url": "js/chunk-vendors~a61ebcbd.63484444.js"
+  },
+  {
+    "revision": "81a6c75e30c8b1fd37ef",
+    "url": "js/chunk-vendors~a6cd21c3.8a30a46c.js"
+  },
+  {
+    "revision": "cd7e7f569f713fe13133",
+    "url": "js/chunk-vendors~b58f7129.4a94c3a6.js"
+  },
+  {
+    "revision": "ee9745cbd34f41aece27",
+    "url": "js/chunk-vendors~b5906859.9b55f318.js"
+  },
+  {
+    "revision": "cf278078ff5b62138c00",
+    "url": "js/chunk-vendors~c1554789.0cebe6e9.js"
+  },
+  {
+    "revision": "b2ed455c00d171ba1e42",
+    "url": "js/chunk-vendors~daa565d3.fa408697.js"
+  },
+  {
+    "revision": "abbbb05c3d2aacecd48d",
+    "url": "js/chunk-vendors~e041e925.0e1fde92.js"
+  },
+  {
+    "revision": "8fc39ac25752e9ea3042",
+    "url": "js/chunk-vendors~e047c002.ad1cfc58.js"
+  },
+  {
+    "revision": "a06430392c4a41935386",
+    "url": "js/chunk-vendors~e4173fa2.b6a0bb65.js"
+  },
+  {
+    "revision": "09877eb7d611896ddfb2",
+    "url": "js/chunk-vendors~f1715247.9af21e1b.js"
+  },
+  {
+    "revision": "7585300875bc2dc69672",
+    "url": "js/chunk-vendors~fdc6512a.80e62795.js"
+  },
+  {
+    "revision": "2448c81d20fadca61992",
+    "url": "js/page-404~b42b143f.1ba79d08.js"
+  },
+  {
+    "revision": "a21c230946206a6a6c2b",
+    "url": "js/page-admin-index~6773c5d6.c535e231.js"
+  },
+  {
+    "revision": "3135291f3b8412bf2b5a",
+    "url": "js/page-admin-nikah-acara~bb94f058.1c7e05eb.js"
+  },
+  {
+    "revision": "9613377fcdbec94dccac",
+    "url": "js/page-admin-nikah-acara~page-admin-nikah-comment~page-admin-nikah-galery~page-admin-nikah-index~page-~d6cec0eb.5799ff25.js"
+  },
+  {
+    "revision": "7269b0126f84ba81b2c2",
+    "url": "js/page-admin-nikah-comment~12f5e1ca.77467e0f.js"
+  },
+  {
+    "revision": "e33909fc6ecdba06e2ac",
+    "url": "js/page-admin-nikah-galery~dd43ac2e.ab3555ae.js"
+  },
+  {
+    "revision": "986e56046df87f69c275",
+    "url": "js/page-admin-nikah-index~417d630f.dfe1f37d.js"
+  },
+  {
+    "revision": "fe27a7fa71b29517fca6",
+    "url": "js/page-admin-nikah-mempelai~c2a8d1ec.3bd0fd6b.js"
+  },
+  {
+    "revision": "171af45fde8ad1a4b523",
+    "url": "js/page-admin-nikah-pria~f5789d91.658668d6.js"
+  },
+  {
+    "revision": "f802d4e88e4d2e71e785",
+    "url": "js/page-admin-nikah-wanita~9ba64b0f.01774444.js"
+  },
+  {
+    "revision": "c6a0164947e781e5ab46",
+    "url": "js/page-login~320619dd.7575704c.js"
+  },
+  {
+    "revision": "0532b9631b0f82dd5856",
+    "url": "js/page-test~fcee1841.f95b6287.js"
+  },
+  {
+    "revision": "8de06593dc73935da8be",
+    "url": "js/page-undangan-index~31ecd969.45869ce1.js"
+  },
+  {
+    "revision": "58403d2c5545a83c1d9639948e084f98",
+    "url": "og.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "7ea103597f7506e6c42286fea3e406a3",
+    "url": "wedding.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.cdfcbb608d640012e5682b573df394cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.3ab57fd37cc425b5f9ac8c5d66928737.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
   console.log(`Workbox is loaded`);
